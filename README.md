e6c906c6bbd76245e12a08fe7df5eb637f4c0229
AIWASS23 <marcelodearaujo677@gmail.com>
2022-03-28 09:46:50 -0300

    Project done

---
+++ README.md
@@ -0,0 +1,45 @@
+# Jogo Pedra Papel Tesoura
+
+    Joginho em terminal para aliviar o stress
+
+## Referência
+
+    - https://docs.swift.org/swift-book/LanguageGuide/TheBasics.html
+    - https://repositorio.ufu.br/bitstream/123456789/14361/1/JogosDigitaisEstrategia.pdf
+    - https://www.challengebasedlearning.org/pt/framework/
+    - https://github.com/AcademyIFCE/Swift-Book/blob/master/GuidedTour/SwiftTour.md
+    - https://www.journaldev.com/19612/swift-readline-swift-print
+
+
+## Documentação da API
+
+    Language used: Swift
+
+    Library used: Foundation 
+
+    Data inputs: 0 (rock), 1 (paper), 2 (scissors)
+
+    Data outputs: 0 (rock), 1 (paper), 2 (scissors)
+
+    Variables and Structures:
+
+    `   struct Item composed of 3 let Strings(name, winner, loser) and a checkVictory function that notifies who won.
+    
+        3 let Item(rock, paper and scissors)
+    
+        1 let possibleMovies array that receives the 3 let Item
+
+        1 boolean var validNumberBetweenZeroAndTwo set to false
+
+    loop: 
+
+        a while with the variable validNumberBetweenZeroAndTwo is used to create the iteration of the game with the user
+
+        the user chooses a number between 0 and 2 associated with a word, and a number associated with the CPU is generated.
+## Apêndice
+
+    https://repositorio.ufu.br/bitstream/123456789/14361/1/JogosDigitaisEstrategia.pdf
+
+## Autores
+
+    - [Marcelo De Araújo](https://www.github.com/AIWASS23)
---
+++ Sources/Jogo/main.swift
@@ -0,0 +1,53 @@
+import Foundation
+
+struct Item {
+    let name: String
+    let winner: String
+    let loser: String
+    
+    func checkVictory(contra opponent: String) -> String {
+        if self.name == opponent{
+            return "Draw"
+        }
+        else if self.winner == opponent {
+            return "You won, because \(self.name) wins from \(opponent)!"
+        }
+        return "Computer won, because \(opponent) wins from \(self.name)"
+    }
+}
+
+let rock = Item(name: "Rock", winner: "Scissors", loser: "Paper")
+let paper = Item(name: "Paper", winner: "Rock", loser: "Scissors")
+let scissors = Item(name: "Scissors", winner: "Paper", loser: "Rock")
+let possibleMoves = [rock, paper, scissors]
+
+print("Rock\nPaper\nScissors\n")
+print("Enter 0 for rock, 1 for paper or 2 for scissors")
+
+var validNumberBetweenZeroAndTwo = false
+
+while !validNumberBetweenZeroAndTwo {
+    let userResponse = readLine()!
+        
+    if let userResponseInInterger = Int(userResponse) {
+        if userResponseInInterger < 3 && userResponseInInterger > -1 {
+            let cpuResponse = Int.random(in: 0...2)
+            let cpuItem = possibleMoves[cpuResponse]
+            let userItem = possibleMoves[userResponseInInterger]
+                            
+            print("Rock!")
+            sleep(1)
+            print("Paper!")
+            sleep(1)
+            print("Scissors!")
+            sleep(1)
+                
+            print("\nYour move: \(userItem.name)")
+            print("CPU move: \(cpuItem.name)")
+                
+            print("\nRESULT.....")
+            print(userItem.checkVictory(contra: cpuItem.name))
+            print("Enter 0 for rock, 1 for paper or 2 for scissors")
+        }
+    }
+}
